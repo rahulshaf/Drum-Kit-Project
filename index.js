61993ace556e1b5654fb475feda7427c3f40ce4b
Rahul Jain <42069292+rahulshaf@users.noreply.github.com>
2023-01-28 16:54:11 +0530

    Add files via upload
    first version

---
+++ index.js
@@ -0,0 +1,62 @@
+/*document.querySelector("button").addEventListener("click",function (){
+    alert("button click");
+});*/
+function playMusic(buttonText){
+    switch(buttonText){
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play(); 
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play(); 
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play(); 
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play(); 
+            break;    
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play(); 
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play(); 
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play(); 
+            break;
+        default:
+            console.log(buttonText);
+
+    }
+    buttonAnimation(buttonText);
+
+}
+
+function buttonAnimation(buttonText){
+    document.querySelector("."+buttonText).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("."+buttonText).classList.remove("pressed");
+
+    },100);
+
+}
+var buttons=document.querySelectorAll(".drum");
+for(var i=0;i<buttons.length;i++){
+    buttons[i].addEventListener("click",function (){
+        var buttonText=this.innerHTML;
+        playMusic(buttonText);
+        
+    });
+}
+document.addEventListener("keypress",function(event){
+    playMusic(event.key);
+})
+
+
